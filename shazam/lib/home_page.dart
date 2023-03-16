5e345ee1d8a6bdd6b03944031db05b320a7c3af1
babeebird <babeebird@naver.com>
2023-03-16 17:30:11 +0900

    shazam

---
+++ shazam/lib/home_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:shazam/second_tab.dart';
+import 'package:shazam/first_tab.dart';
+import 'package:shazam/third_tab.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Builder(builder: (context) {
+        DefaultTabController.of(context)?.addListener(() {
+          setState(() {});
+        });
+        return Scaffold(
+          body: Stack(
+            children: [
+              TabBarView(
+                children: [
+                  FirstTab(),
+                  SecondTab(),
+                  ThirdTab(),
+                ],
+              ),
+              SafeArea(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topCenter,
+                        child: TabPageSelector(
+                          color: DefaultTabController.of(context)?.index == 1
+                              ? Colors.blue[300]
+                              : Colors.grey[400],
+                          selectedColor:
+                              DefaultTabController.of(context)?.index == 1
+                                  ? Colors.white
+                                  : Colors.blue,
+                          indicatorSize: 8,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
---
+++ shazam/lib/second_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondTab extends StatelessWidget {
+  const SecondTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('두번째 페이지'));
+  }
+}
